36ae9586e236c48504aa1ba16cab3d9413a4fce8
anjkulkarni <anjaneyakulkarni98@gmail.com>
2023-07-04 17:52:00 +0530

    adding semi colons

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -1,10 +1,10 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
 app.get("/",(req,res)=>{
-    res.send("Service is up & running")
-})
+    res.send("Service is up & running");
+});
 
 app.listen(8080,()=>{
-    console.log("Server is up")
+    console.log("Server is up");
 });
